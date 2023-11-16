c7278d1ab46e72bff7e873ff113d9ce19c5b2463
Valentin Hutter <valentinhutter1296@gmail.com>
2023-11-16 22:02:27 +0100

    refacto: remove support for diacritics, as they are not allowed in the character set for PAIN format reference elements + update valid chars regex pattern

--- src/main/java/io/inisos/bank4j/validator/constraintvalidators/Iso20022CharacterSetValidator.java
+++ src/main/java/io/inisos/bank4j/validator/constraintvalidators/Iso20022CharacterSetValidator.java
@@ -7,15 +7,9 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class Iso20022CharacterSetValidator implements ConstraintValidator<Iso20022CharacterSet, String> {
-    private boolean allowDiacritics;
-
-    @Override
-    public void initialize(Iso20022CharacterSet constraintAnnotation) {
-        this.allowDiacritics = constraintAnnotation.allowDiacritics();
-    }
-
+    
     @Override
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
-        return Iso20022ReferenceElementValidator.isValidCharacterSet(value, this.allowDiacritics);
+        return Iso20022ReferenceElementValidator.isValidCharacterSet(value);
     }
 }
