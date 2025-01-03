2f1ff154b84f59b0d3acdfdeb1d27a487060a89e
Patrice Blanchardie <patrice.blanchardie@gmail.com>
2025-01-03 14:43:11 +0100

    mandatory DbtrAgt (backport)
    fixes #17

---
+++ src/test/java/io/inisos/bank4j/SchemaValidator.java
@@ -0,0 +1,36 @@
+package io.inisos.bank4j;
+
+import iso.std.iso._20022.tech.xsd.pain_001_001.ObjectFactory;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+public class SchemaValidator {
+
+    public static void validateCreditTransfer(Reader reader) throws IOException, JAXBException, SAXException {
+        try (InputStream resourceAsStream = SchemaValidator.class.getClassLoader().getResourceAsStream("pain.001.001.03.xsd")) {
+
+            JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+            Source xsdFile = new StreamSource(resourceAsStream);
+
+            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = sf.newSchema(xsdFile);
+            jaxbUnmarshaller.setSchema(schema);
+
+            jaxbUnmarshaller.unmarshal(reader);
+        }
+    }
+}
